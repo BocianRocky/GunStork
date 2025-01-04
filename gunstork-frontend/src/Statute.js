d931639d45732bfae1d8668fac97921b6f32c8fa
BocianRocky <mateusz.ro@op.pl>
2025-01-04 15:44:56 +0100

    create Statute

---
+++ gunstork-frontend/src/Statute.js
@@ -0,0 +1,18 @@
+import './styles/Statute.css'
+
+function Statute(){
+
+
+    return(
+        <div className='statute-container'>
+            <div className='statute-settings'>
+                <div className='statute-intro'>
+                    <h2>Regulamin</h2>
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
+export default Statute;
