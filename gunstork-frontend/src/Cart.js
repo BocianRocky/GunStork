ce140670617e7ef622c5f24709dc3258c5fef9fe
BocianRocky <mateusz.ro@op.pl>
2025-01-04 15:44:07 +0100

    remove handleQuantityProduct

--- gunstork-frontend/src/Cart.js
+++ gunstork-frontend/src/Cart.js
@@ -1,26 +1,11 @@
-import { useState } from 'react';
 import './styles/Cart.css';
+import remove from './assets/images/remove.png';
 
-function Cart({cart}){
-
-    const [cartItems,setCartItems]=useState(cart);
-
-    const handleQuantityProduct =(e,productId)=>{
-        const updateProducts=cartItems.map((product)=>{
-            if(product.ProductId===productId){
-                return{...product,Quantity: parseInt(e.target.value,10)}
-            }else{
-                return product;
-            }
-        });
-        setCartItems(updateProducts);
-        
-    };
-
+function Cart({cart, handleRemoveProductFromCart,handleQuantityProduct}){
+    
     const totalPrice=()=>{
-        return cartItems.reduce((sum,product)=>sum+(product.Quantity*product.Price),0).toFixed(2);
+        return cart.reduce((sum,product)=>sum+(product.Quantity*product.Price),0).toFixed(2);
     }
-
 
     return(
         <div className='cart-container'>
@@ -28,44 +13,105 @@ function Cart({cart}){
                     <div className='cart-intro'>
                         <h2>Zawartość Twojego koszyka</h2>
                     </div>
-                {cartItems.length === 0 ? (
+                {cart.length === 0 ? (
                     <p>Twój koszyk jest pusty</p>
                 ) : (
-                    <div className='cart-area'>
-                        <table class='cart-table'>
-                            <thead>
-                                <tr>
-                                    <th>Zdjęcie</th>
-                                    <th>Nazwa produktu</th>
-                                    <th>Usuń</th>
-                                    <th>Cena</th>
-                                    <th>Ilość</th>
-                                    <th>Wartość</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    cartItems.map((product)=>(
-                                        <tr key={product.ProductId}>
-                                            <td className='photo-settings'><img src={require(`./assets/images/${product.Image}`)} alt={product.ProductName}/></td>
-                                            <td>{product.ProductName}</td>
-                                            <td>usuń</td>
-                                            <td className='center'>{product.Price} zł</td>
-                                            <td><input type='number' min='1' value={product.Quantity} onChange={(e)=>handleQuantityProduct(e,product.ProductId)}/></td>
-                                            <td className='center'>{(product.Price*product.Quantity).toFixed(2)} zł</td>
-                                        </tr>
-                                    ))
-                                }
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>Wartość</td>
-                                <td>{totalPrice()} zł</td>
-                            </tbody>
-                        </table>
-                    </div>
-                    
+                    <div className='area'>
+                        <div className='cart-area'>
+                            <table class='cart-table'>
+                                <thead>
+                                    <tr>
+                                        <th>Zdjęcie</th>
+                                        <th>Nazwa produktu</th>
+                                        <th>Usuń</th>
+                                        <th>Cena</th>
+                                        <th>Ilość</th>
+                                        <th>Wartość</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        cart.map((product)=>(
+                                            <tr key={product.ProductId}>
+                                                <td className='photo-settings'><img src={require(`./assets/images/${product.Image}`)} alt={product.ProductName}/></td>
+                                                <td class='table-name-product'>{product.ProductName}</td>
+                                                <td className='center'><button onClick={()=>handleRemoveProductFromCart(product.ProductId)} className='button-remove'><img src={remove} alt='Usuń'/></button></td>
+                                                <td className='center'>{product.Price} zł</td>
+                                                <td><input type='number' min='1' value={product.Quantity} onChange={(e)=>handleQuantityProduct(e,product.ProductId)}/></td>
+                                                <td className='center'>{(product.Price*product.Quantity).toFixed(2)} zł</td>
+                                            </tr>
+                                        ))
+                                    }
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>Wartość</td>
+                                    <td>{totalPrice()} zł</td>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className='delivery-payment-container'>
+                                <div className='delivery-payment-item'>
+                                    <div className='dp-settings-container'>
+                                        <h3>Wybierz kraj dostawy</h3>
+                                        <div className='delivery'>
+                                            <select id='kraj-dostawy' name='kraj-dostawy'>
+                                                <option value='CZ'>Czechy</option>
+                                                <option value='PL' selected>Polska</option>
+                                                <option value='FR'>Francja</option>
+
+                                            </select>
+                                        </div>
+                                        <h3>Wybierz formę wysyłki</h3>
+                                        <div className='radio-list'>
+                                            <label>
+                                                <input type='radio' name='metoda-dostawy' value='pickup' checked/>
+                                                Odbiór osobisty
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='delivery-payment-item'>
+                                    <div className='dp-settings-container'>
+                                        <h3>Płatności</h3>
+                                        <div className='radio-list'>
+                                            <label>
+                                                <input type='radio' name='rodzaj-platnosci' value='a' checked/>
+                                                Płatność gotówką
+                                            </label>
+                                            <label>
+                                                <input type='radio' name='rodzaj-platnosci' value='a' checked/>
+                                                Płatność przelewem
+                                            </label>
+                                            <label>
+                                                <input type='radio' name='rodzaj-platnosci' value='a' checked/>
+                                                Płatność kartą w punkcie odbioru
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='delivery-payment-item'>
+                                    <div className='dp-settings-container'>
+                                        <div className='summary'>
+                                            <h3>Koszty</h3>
+                                            <div className='summary-info'>
+                                                <span>Wartość koszyka:</span><span>{totalPrice()} zł</span>
+                                            </div>
+                                            <div className='summary-info'>
+                                                <span>Koszt wysyłki:</span><span>0.00 zł</span>
+                                            </div>
+                                            <div className='summary-info'>
+                                                <span className='all'>Razem:</span><span className='all'>{totalPrice()} zł</span>
+                                            </div>
+                                            <div className='button-all'>
+                                                <button>Do kasy</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
                 )}
             </div>
         </div>
