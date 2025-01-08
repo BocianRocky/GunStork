116f37acf790b2d60ba47d6b137afca2ba3cdb18
BocianRocky <mateusz.ro@op.pl>
2025-01-08 15:26:10 +0100

    add pagination

--- gunstork-frontend/src/Products.js
+++ gunstork-frontend/src/Products.js
@@ -9,27 +9,39 @@ function Products({addProductToCart}){
     const [products, setProducts]= useState([]);
     const [categories,setCategories]=useState([]);
     const [catName,setCatName]=useState();
+    const [currentPage,setCurrentPage]=useState(1);
+    const [total,setTotal]=useState(0);
+    const [previousLocation,setPreviousLocation]=useState('');
+
     useEffect(()=>{
         const fetchProducts=async ()=>{
+            
             const params=new URLSearchParams(location.search);
             const type=params.get('type');
-            const limit=params.get('limit');
-            const offset=params.get('offset');
+            const limit=params.get('limit') || 12;
+            const offset=(currentPage-1)*limit;
             const caliber = params.get('caliber');
             const producer = params.get('producer');
+            const previousType=new URLSearchParams(previousLocation).get('type');
             setCatName(params.get('type'));
+            
+            if(type!==previousType){
+                setCurrentPage(1);
+                setPreviousLocation(location.search);
+            }
             try{
                 const res=await axios.get('http://localhost:3000/products',{
                     params: {categoryName: type,limit: limit,offset: offset,caliber: caliber, producer:producer},
                 });
-                setProducts(res.data);
+                setProducts(res.data.products);
+                setTotal(res.data.total);
             }catch(err){
                 console.log(`errr ${err}`);
             }
         };
         fetchProducts();
 
-    },[location.search]);
+    },[location.search,currentPage,previousLocation]);
     
     useEffect(()=>{
         const fetchCategories=async ()=>{
@@ -64,6 +76,20 @@ function Products({addProductToCart}){
         return quantity>0;
     };
 
+    const pagination = ()=>{
+        const pages=[];
+        const totalPages=Math.ceil(total/12)
+        for(let i=1;i<=totalPages;i++){
+            const isActive=currentPage===i;
+            pages.push(<button key={i} className={isActive ? 'active' : 'inactive'} onClick={
+                ()=>{setCurrentPage(i); 
+                window.scrollTo(0, 0);
+            }}>{i}</button>);
+        }
+        return pages;
+    }
+    
+
     const filterCaliber=categories.filter((category)=>category.Type==='Caliber');
 
     return(
@@ -84,37 +110,44 @@ function Products({addProductToCart}){
                 ))} 
             </div>
             <div className='products-container'>
-                
-                {
-                    products.map((product)=>(
-                        <div className='product-item'>
-                            <div key={product.ProductId}>     
-                                <div className='image-item'>
-                                    {!isAvailable(product.Quantity) ?
-                                        (<span className='out-of-stock'><b>OUT OF STOCK</b></span>) 
-                                        : product.DiscountPrice>0 ? (<span className='sale'><b>PROMOCJA -{product.DiscountPrice}%</b></span>)
-                                        : null
-                                    }
+                <div className='products-grid'>
+                    {
+                        products.map((product)=>(
+                            <div className='product-item'>
+                                <div key={product.ProductId}>     
+                                    <div className='image-item'>
+                                        {!isAvailable(product.Quantity) ?
+                                            (<span className='out-of-stock'><b>OUT OF STOCK</b></span>) 
+                                            : product.DiscountPrice>0 ? (<span className='sale'><b>PROMOCJA -{product.DiscountPrice}%</b></span>)
+                                            : null
+                                        }
+                                        <Link to={`/products/${product.ProductId}`}>
+                                            <img src={require(`./assets/images/${product.image}`)} alt={product.ProductName}/>
+                                        </Link>
+                                    </div>
                                     <Link to={`/products/${product.ProductId}`}>
-                                        <img src={require(`./assets/images/${product.image}`)} alt={product.ProductName}/>
+                                        <p className='product-name'>{product.ProductName}</p>
                                     </Link>
+                                    <p><span className='producer-info'>Producent: </span><span className='producer-info2'>{product.Producer}</span></p>
+                                    <p>{product.DiscountPrice===0 ? (<span>{product.Price} zł</span>) : (<><span className='old-price'>{product.Price} zł</span>{" "}<span className='new-price'>{(product.Price-product.Price*(product.DiscountPrice/100)).toFixed(2)} zł</span></>)
+                                    }</p>
+                                    {
+                                    isAvailable(product.Quantity) ?
+                                    <button onClick={()=>handleAddProductToCart(product)}>Do koszyka</button> :
+                                    <button disabled className='disabled-button'>Niedostępny</button>
+                                    }
                                 </div>
-                                <Link to={`/products/${product.ProductId}`}>
-                                    <p className='product-name'>{product.ProductName}</p>
-                                </Link>
-                                <p><span className='producer-info'>Producent: </span><span className='producer-info2'>{product.Producer}</span></p>
-                                <p>{product.DiscountPrice===0 ? (<span>{product.Price} zł</span>) : (<><span className='old-price'>{product.Price} zł</span>{" "}<span className='new-price'>{(product.Price-product.Price*(product.DiscountPrice/100)).toFixed(2)} zł</span></>)
-                                }</p>
-                                {
-                                isAvailable(product.Quantity) ?
-                                <button onClick={()=>handleAddProductToCart(product)}>Do koszyka</button> :
-                                <button disabled className='disabled-button'>Niedostępny</button>
-                                }
-                            </div>
-                        </div>    
-                    ))
-                }
+                            </div>    
+                        ))
+                    }
+                </div>
+                <div className='pagination-container'>
+                    <div className='buttons-space'>
+                        {pagination()}
+                    </div>
+                </div>
             </div>
+            
         </div>
     );
 }
