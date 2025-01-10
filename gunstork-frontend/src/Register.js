cd192f4a3e63dd159b277b4b07479b7b4dd7ca97
BocianRocky <mateusz.ro@op.pl>
2025-01-10 13:39:13 +0100

    change Register.js

--- gunstork-frontend/src/Register.js
+++ gunstork-frontend/src/Register.js
@@ -1,8 +1,26 @@
 import './styles/Register.css'
+
 function Register(){
     return(
-        <div>
-            aaaaaaaaaaa
+        <div className='log-reg-container'>
+            <div className='reg-area'>
+                <div className='login-area'>
+                    <h3>Rejestracja</h3>
+                    <form class="register-form" action="/register" method="POST">
+                        <label><input type="text" name="name" placeholder='Imię' required /></label>
+                        <label><input type="text" name="lastName" placeholder='Nazwisko' required /></label>
+                        <label><input type="text" name="login" placeholder='Login' required /></label>
+                        <label><input type="email" name="email" placeholder='E-mail' required /></label>
+                        <label><input type="password" name="password" placeholder='Hasło' required /></label>
+                        <label><input type="date" name="date" required /></label>
+                        <label><input type="text" name="adress" placeholder='Adres' required /></label>
+                        <label><input type="text" name="licence" placeholder='Numer licencji' required /></label>
+                        <button>Załóż konto</button>
+                        
+                    
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
