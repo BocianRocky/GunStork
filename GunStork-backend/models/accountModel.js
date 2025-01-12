4964741a0191b5c0b1c364a94fde2f670c523e55
BocianRocky <mateusz.ro@op.pl>
2025-01-12 18:41:00 +0100

    add login and updateToken

--- GunStork-backend/models/accountModel.js
+++ GunStork-backend/models/accountModel.js
@@ -1,4 +1,5 @@
 const db=require('../database');
+require('dotenv').config();
 
 const Account={
     findExistsAccountByLoginOrEmail: async (login, email)=>{
@@ -6,8 +7,8 @@ const Account={
         return rows;
     },
     createAccount: async (accountData)=>{
-        const query= `INSERT INTO Account (Name, LastName, Login, Email, Role, Password, Salt, DateOfBirth, Adress, LicenseNumber, RefreshToken, RefreshTokenExp)
-        VALUES(?,?,?,?,'user',?,?,?,?,?, NULL, NULL);`
+        const query= `INSERT INTO Account (Name, LastName, Login, Email, Role, Password, Salt, DateOfBirth, Adress, LicenseNumber, AccessToken, RefreshToken, RefreshTokenExp)
+        VALUES(?,?,?,?,'user',?,?,?,?,?, NULL, NULL, NULL);`
         const values=[
             accountData.Name,
             accountData.LastName,
@@ -21,6 +22,18 @@ const Account={
         ]
         const [result]=await db.execute(query,values);
         return result;
+    },
+    login: async (loginOrEmail)=>{
+        const query=`SELECT * FROM Account WHERE Login= ? OR Email=?`;
+        const [rows]=await db.execute(query,[loginOrEmail,loginOrEmail]);
+        return rows;
+    },
+    updateToken: async (accountId, accessToken, refreshToken, refreshTokenExp)=>{
+        
+        const query=`UPDATE Account SET AccessToken= ?, RefreshToken= ?, RefreshTokenExp= ? WHERE AccountId= ?`;
+        const [rows]=await db.execute(query,[accessToken, refreshToken, refreshTokenExp, accountId]);
+        console.log(rows);
+        return rows;
     }
 
 };
