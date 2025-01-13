b09ef6553c10f80b27bd099a14d896a9c02d6ad5
BocianRocky <mateusz.ro@op.pl>
2025-01-13 01:03:31 +0100

    add router to profile

--- GunStork-backend/routes/accountRoutes.js
+++ GunStork-backend/routes/accountRoutes.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const { register }=require('../controllers/accountController');
+const userController=require('../controllers/userController');
+const authenticateToken = require('../middlewares/authenticateToken');
 const router = express.Router();
 
 router.post('/register', register);
+router.get('/profile',authenticateToken,userController.getUserData);
 
 module.exports=router;
