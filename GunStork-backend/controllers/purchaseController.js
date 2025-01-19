c804a16ae909fa7f1abc67162ab85d84b158508c
BocianRocky <mateusz.ro@op.pl>
2025-01-20 00:30:15 +0100

    add purchaseController

---
+++ GunStork-backend/controllers/purchaseController.js
@@ -0,0 +1,16 @@
+const purchaseModel =require('../models/purchaseModel');
+
+const handlePurchase=async (req,res)=>{
+    try{
+        const {PaymentMethod, TotalCost, Products}=req.body;
+        const AccountId=req.user.AccountId;
+        console.log(AccountId, PaymentMethod, TotalCost, Products);
+        const purchaseId=await purchaseModel.createPurchase(AccountId, PaymentMethod, TotalCost, Products);
+        console.log('koszyk: ',purchaseId);
+        res.status(201).json({message:'zamówienie zostalo złożone'});
+
+    }catch(err){
+        res.status(500).json({message:'wystąpił błąd podczas realizacji koszyka'});
+    }
+}
+module.exports = { handlePurchase };
