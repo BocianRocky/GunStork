71e3d04a10914c9f9dd9d0a733c82289d678d12c
BocianRocky <mateusz.ro@op.pl>
2025-01-20 00:31:41 +0100

    add router auth

--- GunStork-backend/routes/productRoutes.js
+++ GunStork-backend/routes/productRoutes.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const router = express.Router();
 const productController=require('../controllers/productController');
-
+const purchaseController=require('../controllers/purchaseController');
+const authenticateToken = require('../middlewares/authenticateToken');
 //router.get('/',productController.getAllProducts);
 router.get('/sale',productController.getOnlyTwoSaleProducts);
 router.get('/newest',productController.getOnlyTwoNewProducts);
 router.get('/',productController.getProductsByCategoryAndCount);
 router.get('/categories',productController.getCategories);
 router.get('/:productId',productController.getProductById);
+router.post('/purchase',authenticateToken,purchaseController.handlePurchase);
+
 
 module.exports=router;
