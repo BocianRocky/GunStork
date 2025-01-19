f00bb2d340292cf56980223dbff1daae9e259e6b
BocianRocky <mateusz.ro@op.pl>
2025-01-20 00:30:51 +0100

    add userController

--- GunStork-backend/controllers/userController.js
+++ GunStork-backend/controllers/userController.js
@@ -11,3 +11,24 @@ exports.getUserData=async (req,res)=>{
         res.status(500).json({ message: 'Błąd pobierania profilu użytkownika' });
     }
 }
+exports.getAllUserWithTotalCostPurchase=async (req,res)=>{
+    try{
+        const users=await userModel.getAllUsers();
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).json({message: 'Błąd pobierania użytkowników'});
+    }
+}
+
+exports.deleteUser=async (req,res)=>{
+    try{
+        const {id}=req.params;
+        if(!id){
+            return res.status(400).json({message: 'Nie podano ID użytkownika'});
+        }
+        await userModel.deleteUser(id);
+        res.status(200).json({message: `Usunięto użytkownika o id: ${id}`});
+    }catch(err){
+        res.status(500).json({message: 'Błąd podczas usuwania użytkownika'})
+    }
+};
