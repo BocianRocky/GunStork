197906cc56c69955d6ffb2342587274dd84f43f4
BocianRocky <mateusz.ro@op.pl>
2025-01-20 00:30:37 +0100

    add purchaseModel

---
+++ GunStork-backend/models/purchaseModel.js
@@ -0,0 +1,22 @@
+const db=require('../database');
+
+const Purchase={
+    createPurchase: async (AccountId, PaymentMethod, TotalCost, Products)=>{
+        try{
+            const [purchaseResult]=await db.execute(`INSERT INTO Purchase (AccountId, PurchaseDate, PaymentMethod, TotalCost)
+            VALUES (?,CURRENT_TIME(),?,?);`,[AccountId,PaymentMethod,TotalCost]);
+
+            const purchaseId=purchaseResult.insertId;
+
+            for(const product of Products){
+                await db.execute(`INSERT INTO OrderedProduct (ProductId, PurchaseId, Quantity)
+                VALUES (?,?,?);`,[product.ProductId,purchaseId,product.Quantity]);
+            }
+            return purchaseId;
+        }catch(err){
+            console.error('blad wstawiania koszyka');
+            throw new Error('Nie udało sie utworzyć za zamówienia.');
+        }
+    }
+}
+module.exports=Purchase;
